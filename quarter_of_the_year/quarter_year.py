d04354891a3f75e16f40626eac4d5cbfc7a3402d
Vitor Ramos <vitor.ramos2759@gmail.com>
2022-11-22 07:25:38 -0300

    new

---
+++ quarter_of_the_year/quarter_year.py
@@ -0,0 +1,9 @@
+def quarter_of(month):
+    if 1 <= month <= 3:
+        return 1
+    if 4 <= month <= 6:
+        return 2
+    if 6 <= month <= 8:
+        return 3
+    if 8 <= month <= 12:
+        return 4
